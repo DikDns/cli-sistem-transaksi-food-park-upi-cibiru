e9fea78702126cd431b10fbe429d5559ba209d05
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2023-12-23 09:44:53 +0700

    add: lihat_kios panel

--- admin_kios.py
+++ admin_kios.py
@@ -16,20 +16,23 @@ def admin_kios_panel():
         print_body("Panel Admin > Mengelola Kios", start="\n")
 
         print_body("Pilih menu sesuai angka:", start="\n")
-        print_body("1. Registrasi Kios")
-        print_body("2. Lihat Kios")
-        print_body("3. Hapus Kios")
-        print_body("4. Kembali")
+        print_body("1. Lihat Semua Kios")
+        print_body("2. Registrasi Kios")
+        print_body("3. Ubah Data Kios")
+        print_body("4. Hapus Kios")
+        print_body("5. Kembali")
 
         pilihan = input("\nMasukkan pilihan:> ")
 
         if pilihan == "1":
-            registrasi_kios()
-        elif pilihan == "2":
             lihat_kios()
+        elif pilihan == "2":
+            registrasi_kios()
         elif pilihan == "3":
-            hapus_kios()
+            edit_kios()
         elif pilihan == "4":
+            hapus_kios()
+        elif pilihan == "5":
             clear_screen()
             break
         else:
@@ -71,6 +74,20 @@ def registrasi_kios():
                 break
 
             print_alert("Gagal mendaftarkan kios", start="\n")
+
+        konfirmasi = input("\nKembali ke Panel Kios? (Y/N):> ")
+
+        if konfirmasi.upper() == "Y":
+            break
+
+
+def lihat_kios():
+    while True:
+        clear_screen()
+
+        data_kios = csv.get(kios_account_path)
+
+        print_data_kios(data_kios)
 
         konfirmasi = input("\nKembali ke Panel Kios? (Y/N):> ")
 
